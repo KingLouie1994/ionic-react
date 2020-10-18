f0a543c7b2d67ab739f384565010b11d18003fe6
Luis Schekerka <luis@Luiss-MacBook-Pro.local>
2020-10-18 11:08:04 +0200

    added functionality to the segments

--- src/components/InputControls.tsx
+++ src/components/InputControls.tsx
@@ -1,13 +1,17 @@
 import React from "react";
 import { IonLabel, IonSegment, IonSegmentButton } from "@ionic/react";
 
-const InputControls: React.FC = () => {
+const InputControls: React.FC<{ selectedValue: "mkg" | "ftlbs"; onSelectValue: (value: "mkg" | "ftlbs") => void }> = (props) => {
+
+    const inputChangeHandler = (event: CustomEvent) => {
+        props.onSelectValue(event.detail.value);
+    }
   return (
-    <IonSegment>
-      <IonSegmentButton>
+    <IonSegment value={props.selectedValue} onIonChange={inputChangeHandler}>
+      <IonSegmentButton value="mkg">
         <IonLabel>m/kg</IonLabel>
       </IonSegmentButton>
-      <IonSegmentButton>
+      <IonSegmentButton value="ftlbs">
         <IonLabel>ft/lbs</IonLabel>
       </IonSegmentButton>
     </IonSegment>
